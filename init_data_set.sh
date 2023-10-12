61c794d5eb129df175ab25a15ce4bc1564dd047f
Xubuntu <xubuntu@xubuntu-2004.linuxvmimages.local>
2023-10-12 13:33:40 -0400

    update

--- init_data_set.sh
+++ init_data_set.sh
@@ -2,4 +2,4 @@ TPV_PATH_DATASET=$1
 TPV_PATH_MNIST_DATASET=$2
 
 ln -s $TPV_PATH_DATASET files
-ln -s $TPV_PATH_MNIST_DATASET other_datasets/mnist_brain_digits/files/
+ln -s $TPV_PATH_MNIST_DATASET ./notebooks/other_datasets/mnist_brain_digits/files
