c44bbf8c4b2027d5a06ccb28677942e6917f84eb
owalid <othmane.el.ayadi@gmail.com>
2023-10-14 15:23:43 +0200

    update

--- init_data_set.sh
+++ init_data_set.sh
@@ -1,5 +1,7 @@
-TPV_PATH_DATASET=$1
+TPV_PATH_PHYSIONET_MOV_DATASET=$1
 TPV_PATH_MNIST_DATASET=$2
+TPV_PATH_SLEEP_PHYSIONET_DATASET=$2
 
-ln -s $TPV_PATH_DATASET files
+ln -s $TPV_PATH_PHYSIONET_MOV_DATASET files
 ln -s $TPV_PATH_MNIST_DATASET ./notebooks/other_datasets/mnist_brain_digits/files
+ln -s $TPV_PATH_SLEEP_PHYSIONET_DATASET ./notebooks/other_datasets/sleep_edf/files
